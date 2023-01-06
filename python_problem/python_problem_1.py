2591c111c08c2a91d2b231cfd72565161bb569b4
minyou2675 <kymyy2675@gmail.com>
2023-01-07 08:00:29 +0900

    1-3

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,11 @@
 num = 0
-input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+while(1):
+    if not isinstance(num,int):
+        print("정수를 입력하세요")
+        num = map(int,input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    elif num not in list(range(1,4)):
+        print("1,2,3 중 하나를 입력하세요")
+        num = map(int,input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    else:
+        break
