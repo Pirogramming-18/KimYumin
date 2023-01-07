8210f4692eeed48fc84c7688cfd3be034085c272
minyou2675 <kymyy2675@gmail.com>
2023-01-07 22:24:51 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,46 +1,28 @@
 num = 0
-while(num < 31):
-    check = num
-    while(1):
-        a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if not isinstance(a,int):
-            print("정수를 입력하세요")
-            continue
-        elif a not in list(range(1,4)):
-            print("1,2,3 중 하나를 입력하세요")
-            continue
-       
-        else:
-            num += a
-            break
-   
-    for i in range(check+1,num+1):
-        print("playerA :",i)
-    
-    if 31 <= num:
-            print("playerB win!")
-            break 
-            
-    check = num
-   
-    while(1):
-        a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-        if not isinstance(a,int):
-            print("정수를 입력하세요")
-            continue
-        elif a not in list(range(1,4)):
-            print("1,2,3 중 하나를 입력하세요")
-            continue
-       
-        else:
-            num += a
-            break
-        
-    if 31 <= num:
-            print("playerA win!")
-            break
+def brGame():
 
-    for i in range(check+1,num+1):
-        print("playerB :",i)
+    while(num < 31):
+        check = num
+        while(1):
+            a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+            if not isinstance(a,int):
+                print("정수를 입력하세요")
+                continue
+            elif a not in list(range(1,4)):
+                print("1,2,3 중 하나를 입력하세요")
+                continue
         
+            else:
+                num += a
+                break
+    
+        for i in range(check+1,num+1):
+            print("playerA :",i)
+        
+        if 31 <= num:
+                print("playerB win!")
+                break 
+                
+        check = num
+    
   
