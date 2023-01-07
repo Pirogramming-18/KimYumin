1db26277eef235da4224ba0d5da429a33cd1ce3b
minyou2675 <kymyy2675@gmail.com>
2023-01-07 22:20:26 +0900

    1-7

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -9,13 +9,18 @@ while(num < 31):
         elif a not in list(range(1,4)):
             print("1,2,3 중 하나를 입력하세요")
             continue
+       
         else:
             num += a
             break
    
     for i in range(check+1,num+1):
         print("playerA :",i)
-        
+    
+    if 31 <= num:
+            print("playerB win!")
+            break 
+            
     check = num
    
     while(1):
@@ -26,10 +31,16 @@ while(num < 31):
         elif a not in list(range(1,4)):
             print("1,2,3 중 하나를 입력하세요")
             continue
+       
         else:
             num += a
+            break
+        
+    if 31 <= num:
+            print("playerA win!")
             break
 
     for i in range(check+1,num+1):
         print("playerB :",i)
-    
+        
+  
