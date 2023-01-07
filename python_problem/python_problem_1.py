2697804d3c41d56a026d31f1b38ee3683c9b289e
minyou2675 <kymyy2675@gmail.com>
2023-01-07 22:05:17 +0900

    1-5

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,14 +1,31 @@
 num = 0
-num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+
 while(1):
-    if not isinstance(num,int):
+    a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    if not isinstance(a,int):
         print("정수를 입력하세요")
-        num = map(int,input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-    elif num not in list(range(1,4)):
+        continue
+    elif a not in list(range(1,4)):
         print("1,2,3 중 하나를 입력하세요")
-        num = map(int,input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        continue
     else:
+        num += a
         break
 
-for i in range(1,num):
-    playerA: i
+for i in range(1,num+1):
+    print("playerA :",i)
+num2 = 0 
+while(1):
+    a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+    if not isinstance(a,int):
+        print("정수를 입력하세요")
+        continue
+    elif a not in list(range(1,4)):
+        print("1,2,3 중 하나를 입력하세요")
+        continue
+    else:
+        num2 += num+a
+        break
+
+for i in range(num+1,num2+1):
+    print("playerB :",i)
