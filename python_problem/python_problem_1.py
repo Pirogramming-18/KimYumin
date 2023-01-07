f8253bab03de465e7cb80c831288894a6b59428f
minyou2675 <kymyy2675@gmail.com>
2023-01-07 22:13:15 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,31 +1,35 @@
 num = 0
+while(num < 31):
+    check = num
+    while(1):
+        a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if not isinstance(a,int):
+            print("정수를 입력하세요")
+            continue
+        elif a not in list(range(1,4)):
+            print("1,2,3 중 하나를 입력하세요")
+            continue
+        else:
+            num += a
+            break
+   
+    for i in range(check+1,num+1):
+        print("playerA :",i)
+        
+    check = num
+   
+    while(1):
+        a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+        if not isinstance(a,int):
+            print("정수를 입력하세요")
+            continue
+        elif a not in list(range(1,4)):
+            print("1,2,3 중 하나를 입력하세요")
+            continue
+        else:
+            num += a
+            break
 
-while(1):
-    a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-    if not isinstance(a,int):
-        print("정수를 입력하세요")
-        continue
-    elif a not in list(range(1,4)):
-        print("1,2,3 중 하나를 입력하세요")
-        continue
-    else:
-        num += a
-        break
-
-for i in range(1,num+1):
-    print("playerA :",i)
-num2 = 0 
-while(1):
-    a = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
-    if not isinstance(a,int):
-        print("정수를 입력하세요")
-        continue
-    elif a not in list(range(1,4)):
-        print("1,2,3 중 하나를 입력하세요")
-        continue
-    else:
-        num2 += num+a
-        break
-
-for i in range(num+1,num2+1):
-    print("playerB :",i)
+    for i in range(check+1,num+1):
+        print("playerB :",i)
+    
